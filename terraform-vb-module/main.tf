ac4916907c81c199893b5e09ac3b6f2bd556f0b3
Kode <kodelkeyz13@gmail.com>
2023-11-11 20:16:19 +0100

    Minor changes in Terraform file

--- terraform-vb-module/main.tf
+++ terraform-vb-module/main.tf
@@ -2,18 +2,18 @@ terraform {
   required_providers {
     virtualbox = {
         source = "terra-farm/virtualbox"
-        version = "0.2.1"
+        version = "0.2.2-alpha.1"
     }
   }
 }
 
-resource "vurtualbox_vm" "node"{
-    count = 2
-    name = "testvm"count.index + 1
-    image = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
-    cpus = 4
+resource "virtualbox_vm" "node"{
+    count  = 2
+    name   = format("node-%02d", count.index + 1)
+    image  = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+    cpus   = 4
     memory = "512 mib"
-    user_data = file("${path.module}/user_data")
+
 
     network_adapter{
         type = "hostonly"
